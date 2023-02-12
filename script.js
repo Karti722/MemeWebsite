a49da12a7f3a85595ca13a7d84872dddf357b5e1
Karti722 <98507922+Karti722@users.noreply.github.com>
2023-02-11 19:10:20 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let randomNum = Math.round(Math.random()) % 2;
-let meme = document.getElementById("meme");
-let title = document.getElementById("title");
-let getMemeBtn = document.getElementById("get-meme-btn");
+var randomNum = Math.round(Math.random()) % 2;
+var meme = document.getElementById("meme");
+var title = document.getElementById("title");
+var getMemeBtn = document.getElementById("get-meme-btn");
 // API URL
-let url = "https://meme-api.com/gimme/";
+var url = "https://meme-api.com/gimme/";
 // Array of subreddits
-let subreddits = ["wholesomememes", "catmemes", "dogmemes", "me_irl"];
+var subreddits = ["wholesomememes", "catmemes", "dogmemes", "me_irl"];
 // Function to Get Random Memem
-let getMeme = function(){
+var getMeme = function(){
     // Choose a random subreddit from the subreddits array
     let randomSubreddit = subreddits[Math.floor(Math.random() * subreddits.length)];
     // console.log(randomSubreddit);
