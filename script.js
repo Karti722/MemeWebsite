954ddef5bb2a339e4e7c5ff83d3563d52da59ada
Karti722 <98507922+Karti722@users.noreply.github.com>
2023-02-11 18:40:42 -0800

    Uploaded files from Desktop.
    Uploaded files from my computer. The code was prewritten.

---
+++ script.js
@@ -0,0 +1,41 @@
+let randomNum = Math.round(Math.random());
+document.body.style.backgroundImage = "url('background-images/app-background" + randomNum + ".png')";
+
+let meme = document.getElementById("meme");
+let title = document.getElementById("title");
+let getMemeBtn = document.getElementById("get-meme-btn");
+// API URL
+let url = "https://meme-api.com/gimme/";
+// Array of subreddits
+let subreddits = ["wholesomememes", "catmemes", "dogmemes", "me_irl"];
+
+// Function to Get Random Memem
+let getMeme = function(){
+    // Choose a random subreddit from the subreddits array
+    let randomSubreddit = subreddits[Math.floor(Math.random() * subreddits.length)];
+    // console.log(randomSubreddit);
+    // Fetch data from the api
+    fetch(url + randomSubreddit)
+    .then((resp) => resp.json())
+    .then((data) => {
+        // console.log(data);
+        let memeImg = new Image();
+        // Display meme image and title only after the image is loaded
+        memeImg.onload = () => {
+            meme.src = data.url;
+            title.innerHTML = data.title;
+        };
+        memeImg.src = data.url;
+    });
+};
+
+// Call the getMeme() on button click 
+// Changes the background image by changing the randomNum value and changing the background image url accordingly
+getMemeBtn.addEventListener("click", function () {
+    getMeme();
+    randomNum = Math.round(Math.random());
+    document.body.style.backgroundImage = "url('background-images/app-background" + randomNum + ".png')";
+});
+
+
+window.addEventListener("load", getMeme);
